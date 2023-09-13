a746b009b436561f534e0db3082a3ac46d4838c8
In-hyuk <dlsgur109@naver.com>
2023-09-13 23:12:21 +0900

    delete

--- C_Project1Jung/C_Project1/id.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-void saveUsers(struct User users[], int numUsers);
-extern int loadUsers(struct User users[]);
-extern int checkID(struct User users[], int numUsers, const char* id);
-extern int main();
